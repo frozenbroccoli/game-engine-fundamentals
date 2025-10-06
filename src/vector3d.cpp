6809df51c5da527260a88bf735aa9e041c864427
Souvik <shouvik0104@gmail.com>
2025-10-06 21:18:57 +0530

    addition and subtraction of Vector3D

--- src/vector3d.cpp
+++ src/vector3d.cpp
@@ -8,18 +8,18 @@ struct Vector3D {
     Vector3D(float a, float b, float c) : x{a}, y{b}, z{c} {}
 
     float& operator [](int i) {
-        return ((&x)[i]);
+        return (&x)[i];
     }
 
     const float& operator [](int i) const {
-        return ((&x)[i]);
+        return (&x)[i];
     }
 
     Vector3D& operator *=(float s) {
         x *= s;
         y *= s;
         z *= s;
-        return (*this);
+        return *this;
     }
 
     Vector3D& operator /=(float s) {
@@ -27,7 +27,21 @@ struct Vector3D {
         x *= s;
         y *= s;
         z *= s;
-        return (*this);
+        return *this;
+    }
+
+    Vector3D& operator +=(const Vector3D& v) {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return *this;
+    }
+
+    Vector3D& operator -=(const Vector3D& v) {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return *this;
     }
 };
 
@@ -42,6 +56,14 @@ inline Vector3D operator /(const Vector3D& v, float s) {
 
 inline Vector3D operator -(const Vector3D& v) {
     return Vector3D(-v.x, -v.y, -v.z);
+}
+
+inline Vector3D operator +(const Vector3D& a, const Vector3D& b) {
+    return Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+inline Vector3D operator -(const Vector3D& a, const Vector3D& b) {
+    return Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 
 inline float Magnitude(const Vector3D& v) {
